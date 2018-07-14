2377adb89c56068fa018689bdb4fd916e5875746
Satya Bose <satyabose.uiux@gmail.com>
2018-07-14 15:43:35 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Benny's Beauty and Hair Care - Contact</title>
+<title>Contact</title>
 <link href="css/global.css" rel="stylesheet" type="text/css" />
 <script language="javascript" type="text/javascript" src="validations.js"></script>
 <script language="javascript" type="text/javascript">
